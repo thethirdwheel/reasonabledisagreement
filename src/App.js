f60ccda8da637ab1f43305f8064455b4c45bc28a
Rory Carmichael <rcarmichael@quantcast.com>
2016-12-03 15:56:55 -0800

    Simplify site. Remove nonfunctional cruft. Change soundlcoud embed format to show show icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Footer, Grid, Cell, List, ListItem, Layout, Header, Navigation, Content, Card, CardTitle, CardText, } from 'react-mdl';
+import { Icon, FABButton, Footer, Grid, Cell, List, ListItem, Layout, Header, Navigation, Content, Card, CardTitle, CardText, } from 'react-mdl';
+
+var pallet = {
+  black: "#343330",
+  white: "#F2F4F3",
+  lightBlue: "#2660A4",
+  darkBlue: "#384E77",
+  orange: "#F19953",
+}
 
 var SoundCloudEmbed = React.createClass({
   render : function() {
@@ -9,13 +17,13 @@ var SoundCloudEmbed = React.createClass({
       padding: 10,
       marginLeft: 10, 
       marginRight: 10, 
-      backgroundColor: "#F2F4F3",
-      width: 800,
+      backgroundColor: pallet["white"],
+      width: '80%',
     };
 
     return (
       <div style={embedStyle}>
-        <iframe width="800" height="150" scrolling="no" frameBorder="no" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + this.props.trackNumber + "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"}></iframe>
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + this.props.trackNumber + "&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"}></iframe>
       </div>
     )
   }
@@ -25,8 +33,8 @@ var EpisodeCard = React.createClass({
   render : function() {
     return (
       <div>
-        <Card shadow={0} style={{width: '850px', margin: 'auto', backgroundColor: "#F2F4F3"}}>
-          <CardTitle style={{backgroundColor: '#F19953', color: '#343330'}}>Episode {this.props.number} - {this.props.title}</CardTitle>
+        <Card shadow={0} style={{width: '850px', margin: 'auto', backgroundColor: pallet["white"]}}>
+          <CardTitle style={{backgroundColor: pallet["darkBlue"], color: pallet["white"]}}>Episode {this.props.number} - {this.props.title}</CardTitle>
           <SoundCloudEmbed trackNumber={this.props.trackNumber} />
           <CardText>
             {this.props.children}
@@ -41,19 +49,21 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Header style={{backgroundColor: "#343330", color: "#F2F43"}}title="Reasonable Disagreement" scroll>
+        <Header style={{backgroundColor: pallet["black"], color: pallet["white"]}}title="Reasonable Disagreement" scroll>
           <Navigation>
-            <a href="">Link</a>
+            <FABButton style={{backgroundColor: pallet["orange"], color: pallet["white"]}}>
+              <Icon name="home" />
+            </FABButton>
           </Navigation>
         </Header>
-        <Content>
-          <Grid noSpacing>
-            <Cell style={{backgroundColor: "#F2F43"}} component={'aside'} className="sidebar-components mdl-shadow--4dp" col={2}>
+        <Content style={{backgroundColor: pallet["white"]}}>
+          {/*<Grid noSpacing>
+                      <Cell style={{backgroundColor: "#F2F43"}} component={'aside'} className="sidebar-components mdl-shadow--4dp" col={2}>
               <List>
                 <ListItem> <a href="">Link</a> </ListItem>
               </List>
             </Cell>
-            <Cell col={10}>
+            <Cell col={10}>*/}
               <List>
                 <ListItem>
                   <EpisodeCard number="1" title="Cultural Appropriation" trackNumber="290921819">
@@ -62,10 +72,10 @@ class App extends Component {
                   </EpisodeCard>
                 </ListItem>
               </List>
-            </Cell>
-          </Grid>
+              {/*</Cell>
+          </Grid>*/}
         </Content>
-        <Footer style={{backgroundColor: "#343330"}}>
+        <Footer style={{backgroundColor: pallet["black"]}}>
         </Footer>
       </Layout>
     );
