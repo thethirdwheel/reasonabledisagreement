f80d22ead1fe549179b62d15aa21ecf573201d28
Rory Carmichael <rcarmichael@quantcast.com>
2016-12-06 20:10:41 -0800

    Okay, I really didn't like the trailing colons

--- src/App.js
+++ src/App.js
@@ -69,8 +69,8 @@ class App extends Component {
                     <b>Show Notes</b>
                     <br/>
                     <br/>
-                    <i>2:32:</i> Kevin is referring to Brandolini's law: "The amount of energy needed to refute bullshit is an order of magnitude bigger than to produce it.": <a href="http://ordrespontane.blogspot.fr/2014/07/brandolinis-law.html">[1]</a> <br/>
-                    <i>18:40:</i> The sushi crusade took place at Oberlin: <a href="http://www.theatlantic.com/politics/archive/2015/12/the-food-fight-at-oberlin-college/421401/">[2]</a><br/>
+                    <i>2:32:</i> Kevin is referring to Brandolini's law: "The amount of energy needed to refute bullshit is an order of magnitude bigger than to produce it."<a href="http://ordrespontane.blogspot.fr/2014/07/brandolinis-law.html">[1]</a> <br/>
+                    <i>18:40:</i> The sushi crusade took place at Oberlin.<a href="http://www.theatlantic.com/politics/archive/2015/12/the-food-fight-at-oberlin-college/421401/">[2]</a><br/>
                     <br/>
                     <br/>
                     You can find Rory's position essay <a href="https://docs.google.com/document/d/1gLkSHE-DZAYJPr_3WHASJ2uDIL7Mlaq4JyQYAQPo54s/edit?usp=sharing">here</a>. 
